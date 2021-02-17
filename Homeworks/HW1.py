469a1ca2250511131deb9598b5d7f87bfb7d8dfe
erenalpt <65376830+erenalpt@users.noreply.github.com>
2021-02-17 21:26:56 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,15 @@
-#Explain your work
+import random
+from sympy import randprime
 
-#Question 1
-for x in range(a):
-	print(a)
+rndPrmMatrix = [[random.choice([x for x in range(2, 1000) if not [t for t in range(2, x) if not x % t]]) for x in range(3)] for x in range(3)]
+
+# for sympy
+# rndPrmMatrix = [[randprime(0, 100) for x in range(3)] for x in range(3)]
+
+
+print(rndPrmMatrix)
+
+#for i in range(3):
+#    print(rndPrmMatrix[i])
+
+# Exam Output: [[821, 163, 643], [467, 67, 563], [971, 109, 349]]
