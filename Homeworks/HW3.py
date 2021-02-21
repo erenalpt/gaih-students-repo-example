d957b6039eea24cb2e37408821371f89fedace90
erenalpt <65376830+erenalpt@users.noreply.github.com>
2021-02-21 13:44:50 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,23 @@
-#Explain your work
+def prime_first(num):
+    if num<501:
+        if not num<2:
+            for i in range(2, num):
+                if num%i == 0:
+                    break
+            else:
+                print(num)
 
-#Question 1
-for x in range(a):
-	print(a)
+
+def prime_second(num):
+    if num>500 and num <1001:
+        if not num<2:
+            for i in range(2, num):
+                if num%i == 0:
+                    break
+            else:
+                print(num)
+        
+
+for i in range(1,1000):
+    prime_first(i)
+    prime_second(i)
